0ebbb25f1b51aed75617fb7eb4f80b149d1a2a8f
Javernaut <javernaut@gmail.com>
2019-04-07 18:45:41 +0200

    Video Preview. Creating a dummy SurfaceView that will show an actual preview

---
+++ app/src/main/java/com/javernaut/whatthecodec/FrameSurfaceView.java
@@ -0,0 +1,11 @@
+package com.javernaut.whatthecodec;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class FrameSurfaceView extends SurfaceView {
+    public FrameSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
