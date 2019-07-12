f053fcaf1c6d5a1cfb61cd9cdc3355aec381f324
Javernaut <javernaut@gmail.com>
2019-07-12 11:20:44 +0300

    Displaying 4 frames simultaneously

--- app/src/main/java/com/javernaut/whatthecodec/FrameDisplayingView.kt
+++ app/src/main/java/com/javernaut/whatthecodec/FrameDisplayingView.kt
@@ -3,28 +3,49 @@ package com.javernaut.whatthecodec
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.util.AttributeSet
-import android.widget.ImageView
+import android.view.View
+import kotlin.math.sqrt
 
-class FrameDisplayingView(context: Context, attrs: AttributeSet) : ImageView(context, attrs) {
+class FrameDisplayingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+    private var frames: Array<Bitmap>? = null
+        set(value) {
+            field = value
+            // TODO consider view size change in order to support different aspect rations of frames
+            invalidate()
+        }
 
     // TODO move this functionality to a background thread
     fun setVideoConfig(config: VideoFileConfig) {
         val bitmaps = Array<Bitmap>(4) {
             Bitmap.createBitmap(
-                    resources.getDimensionPixelSize(R.dimen.preview_width),
-                    resources.getDimensionPixelSize(R.dimen.preview_height),
+                    // TODO consider sizes relatively to the view size itself and spaces between frames
+                    resources.getDimensionPixelSize(R.dimen.preview_width) / 2,
+                    resources.getDimensionPixelSize(R.dimen.preview_height) / 2,
                     Bitmap.Config.ARGB_8888)
         }
         config.fillWithPreview(bitmaps)
-        setImageBitmap(bitmaps.last())
+        frames = bitmaps
     }
 
     fun showLauncherIcon() {
         val bitmap = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
         doTheThingWithBitmap(bitmap)
-        setImageBitmap(bitmap)
+        frames = arrayOf(bitmap)
     }
 
     private external fun doTheThingWithBitmap(bitmap: Bitmap)
+
+    override fun onDraw(canvas: Canvas) {
+        frames?.let {
+            val bitmapsPerRow = sqrt(it.size.toDouble()).toInt()
+            it.forEachIndexed { index, bitmap ->
+                val left = index.rem(bitmapsPerRow) * bitmap.width
+                val top = index / bitmapsPerRow * bitmap.height
+                canvas.drawBitmap(bitmap, left.toFloat(), top.toFloat(), null)
+            }
+        }
+    }
 }
