f9c4ce917ddeed2b93bf2f8d0c0aacf20ed3ea94
Javernaut <javernaut@gmail.com>
2019-11-14 18:33:18 +0200

    Adding a test for an audio file

--- app/src/androidTest/java/com/javernaut/whatthecodec/MediaFileTest.kt
+++ app/src/androidTest/java/com/javernaut/whatthecodec/MediaFileTest.kt
@@ -14,17 +14,18 @@ import java.io.File
 @RunWith(AndroidJUnit4::class)
 class MediaFileTest {
 
-    private val testFileName = "test_video.mkv"
+    private val testVideoFileName = "test_video.mkv"
+    private val testAudioFileName = "test_audio.aac"
 
     @Test
-    fun testMediaFileAccessors() {
+    fun testVideoFile() {
         val context = InstrumentationRegistry.getInstrumentation().context
         val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
 
         // Writing a video file from assets to internal memory
-        val inputStream = context.assets.open(testFileName)
+        val inputStream = context.assets.open(testVideoFileName)
 
-        val testFile = File(targetContext.filesDir, testFileName)
+        val testFile = File(targetContext.filesDir, testVideoFileName)
         testFile.createNewFile()
 
         val outputStream = testFile.outputStream()
@@ -80,9 +81,63 @@ class MediaFileTest {
         assertThat(subtitleStream.language).isEqualTo("eng")
         assertThat(subtitleStream.disposition).isEqualTo(0)
 
+        // Clean up
         mediaFile.release()
 
+        testFile.delete()
+    }
+
+    @Test
+    fun testAudioFile() {
+        val context = InstrumentationRegistry.getInstrumentation().context
+        val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+
+        // Writing a video file from assets to internal memory
+        val inputStream = context.assets.open(testAudioFileName)
+
+        val testFile = File(targetContext.filesDir, testAudioFileName)
+        testFile.createNewFile()
+
+        val outputStream = testFile.outputStream()
+        inputStream.copyTo(outputStream)
+
+        outputStream.flush()
+        outputStream.close()
+        inputStream.close()
+
+        // Actual test
+        val descriptor = targetContext.contentResolver.openFileDescriptor(Uri.parse("file://" + testFile.absolutePath), "r")
+
+        val mediaFile = MediaFileBuilder(MediaType.AUDIO).from(descriptor!!).create()
+
+        assertThat(mediaFile).isNotNull()
+
+        // Video stream
+        assertThat(mediaFile!!.videoStream).isNull()
+
+        // Audio stream
+        assertThat(mediaFile.audioStreams).isNotNull()
+        assertThat(mediaFile.audioStreams).hasSize(1)
+
+        val audioStream = mediaFile.audioStreams.first()
+        assertThat(audioStream.index).isEqualTo(0)
+        assertThat(audioStream.codecName).isEqualTo("AAC (Advanced Audio Coding)")
+        assertThat(audioStream.title).isNull()
+        assertThat(audioStream.language).isNull()
+        assertThat(audioStream.bitRate).isEqualTo(98625)
+        assertThat(audioStream.sampleFormat).isEqualTo("fltp")
+        assertThat(audioStream.sampleRate).isEqualTo(48000)
+        assertThat(audioStream.channels).isEqualTo(1)
+        assertThat(audioStream.channelLayout).isEqualTo("mono")
+        assertThat(audioStream.disposition).isEqualTo(0)
+
+        // Subtitle stream
+        assertThat(mediaFile.subtitleStreams).isNotNull()
+        assertThat(mediaFile.subtitleStreams).isEmpty()
+
         // Clean up
+        mediaFile.release()
+
         testFile.delete()
     }
 
