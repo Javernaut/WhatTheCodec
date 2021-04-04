23e6cf3780b49518b98a26da10574a38d659de0f
Javernaut <javernaut@gmail.com>
2021-04-04 17:24:47 +0300

    Displaying a dropdown menu with the compose

--- app/src/main/java/com/javernaut/whatthecodec/presentation/stream/adapter/StreamFeatureViewHolder.kt
+++ app/src/main/java/com/javernaut/whatthecodec/presentation/stream/adapter/StreamFeatureViewHolder.kt
@@ -2,19 +2,13 @@ package com.javernaut.whatthecodec.presentation.stream.adapter
 
 import android.content.ClipData
 import android.content.ClipboardManager
-import android.text.Spannable
-import android.text.SpannableString
-import android.text.style.ForegroundColorSpan
-import android.util.TypedValue
+import android.content.Context
 import android.widget.Toast
-import androidx.appcompat.widget.PopupMenu
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.colorResource
@@ -32,61 +26,32 @@ class StreamFeatureViewHolder(private val itemComposeView: ComposeView) :
 
     private lateinit var streamFeature: StreamFeature
 
-    init {
-        itemComposeView.setOnLongClickListener {
-            showPopupMenu()
-            true
-        }
-    }
-
     fun bindTo(streamFeature: StreamFeature) {
         this.streamFeature = streamFeature
 
         itemComposeView.setContent {
             WhatTheCodecTheme {
+                var expanded by remember { mutableStateOf(false) }
                 StreamFeature(streamFeature = streamFeature) {
-                    showPopupMenu()
+                    expanded = true
+                }
+                CopyToClipboardDropdown(expanded, streamFeature.description) {
+                    expanded = false
                 }
             }
         }
     }
+}
 
-    private fun showPopupMenu() {
-        val popupMenu = PopupMenu(itemComposeView.context, itemComposeView)
-
-        val firstItemText: Spannable = SpannableString(streamFeature.description)
-        val accentColor = itemComposeView.run {
-            val typedValue = TypedValue()
-            context.theme.resolveAttribute(R.attr.colorAccent, typedValue, true)
-            typedValue.data
-        }
-        firstItemText.setSpan(
-            ForegroundColorSpan(accentColor),
-            0, firstItemText.length,
-            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-        )
-
-        val firstItem = popupMenu.menu.add(firstItemText)
-        firstItem.isEnabled = false
-
-        popupMenu.menu.add(R.string.stream_copy).setOnMenuItemClickListener {
-            copyTextToClipboard();
-            true
-        }
-        popupMenu.show()
-    }
-
-    private fun copyTextToClipboard() {
-        val clipboardManager = itemComposeView.context.getSystemService<ClipboardManager>()!!
-        val label = itemComposeView.resources.getString(streamFeature.title)
-        clipboardManager.setPrimaryClip(
-            ClipData.newPlainText(label, streamFeature.description)
-        )
-        val toastMessage = itemComposeView.resources.getString(
-            R.string.stream_text_copied_pattern, streamFeature.description
-        )
-        Toast.makeText(itemComposeView.context, toastMessage, Toast.LENGTH_SHORT).show()
-    }
+private fun copyTextToClipboard(context: Context, valueToCopy: String) {
+    val clipboardManager = context.getSystemService<ClipboardManager>()!!
+    clipboardManager.setPrimaryClip(
+        ClipData.newPlainText(valueToCopy, valueToCopy)
+    )
+    val toastMessage = context.getString(
+        R.string.stream_text_copied_pattern, valueToCopy
+    )
+    Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show()
 }
 
 @ExperimentalFoundationApi
@@ -95,12 +60,17 @@ class StreamFeatureViewHolder(private val itemComposeView: ComposeView) :
 fun PreviewStreamFeature() {
     WhatTheCodecTheme {
         Surface {
-            StreamFeature(
-                streamFeature = StreamFeature(
-                    R.string.page_audio_codec_name,
-                    "Some value"
-                )
-            ) {}
+            val streamFeature = StreamFeature(
+                R.string.page_audio_codec_name,
+                "Some value"
+            )
+            var expanded by remember { mutableStateOf(false) }
+            StreamFeature(streamFeature = streamFeature) {
+                expanded = true
+            }
+            CopyToClipboardDropdown(expanded, streamFeature.description) {
+                expanded = false
+            }
         }
     }
 }
@@ -110,28 +80,46 @@ fun PreviewStreamFeature() {
 fun StreamFeature(
     modifier: Modifier = Modifier,
     streamFeature: StreamFeature,
-    clickListener: () -> Unit
+    longClickListener: () -> Unit
 ) {
-    WhatTheCodecTheme {
-        Column(
-            modifier
-                .combinedClickable(onLongClick = clickListener, onClick = {})
-                .fillMaxWidth()
-                .defaultMinSize(minHeight = 58.dp)
-                .padding(horizontal = 16.dp, vertical = 8.dp)
-        ) {
-            Text(
-                stringResource(id = streamFeature.title).toUpperCase(),
-                style = MaterialTheme.typography.caption
-            )
-            Spacer(Modifier.height(8.dp))
-            Text(
-                streamFeature.description,
-                style = MaterialTheme.typography.body1
-            )
+    Column(
+        modifier
+            .combinedClickable(onLongClick = longClickListener, onClick = {})
+            .fillMaxWidth()
+            .defaultMinSize(minHeight = 58.dp)
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Text(
+            stringResource(id = streamFeature.title).toUpperCase(),
+            style = MaterialTheme.typography.caption
+        )
+        Spacer(Modifier.height(8.dp))
+        Text(
+            streamFeature.description,
+            style = MaterialTheme.typography.body1
+        )
+    }
+}
+
+@Composable
+fun CopyToClipboardDropdown(expanded: Boolean, valueToCopy: String, dismissCallback: () -> Unit) {
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = dismissCallback
+    ) {
+        DropdownMenuItem(enabled = false, onClick = { }) {
+            Text(valueToCopy, color = MaterialTheme.colors.primary)
+        }
+        DropdownMenuItem(onClick = {
+//                val context = LocalContext.current
+//                copyTextToClipboard(context, "ValueToCopy")
+            dismissCallback()
+        }) {
+            Text(stringResource(id = R.string.stream_copy))
         }
     }
 }
+
 
 @Composable
 fun WhatTheCodecTheme(content: @Composable () -> Unit) {
@@ -141,6 +129,9 @@ fun WhatTheCodecTheme(content: @Composable () -> Unit) {
             MaterialTheme.typography.caption.copy(
                 color = colorResource(id = R.color.secondary_text_default_material_light)
             )
+        ),
+        colors = MaterialTheme.colors.copy(
+            primary = colorResource(id = R.color.brand_blue)
         ),
         content = content
     )
