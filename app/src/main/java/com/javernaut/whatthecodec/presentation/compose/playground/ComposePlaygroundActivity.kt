f7c4d7adad6e20b302b512383b2b992158ee998d
Javernaut <javernaut@gmail.com>
2021-04-25 21:37:48 +0300

    Adding a ViewPager analog to ComposePlaygroundActivity

--- app/src/main/java/com/javernaut/whatthecodec/presentation/compose/playground/ComposePlaygroundActivity.kt
+++ app/src/main/java/com/javernaut/whatthecodec/presentation/compose/playground/ComposePlaygroundActivity.kt
@@ -3,26 +3,74 @@ package com.javernaut.whatthecodec.presentation.compose.playground
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Alignment
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import com.google.accompanist.pager.*
 import com.javernaut.whatthecodec.presentation.compose.theme.WhatTheCodecTheme
-import com.javernaut.whatthecodec.presentation.root.ui.EmptyScreen
+import kotlinx.coroutines.launch
 
+@ExperimentalPagerApi
 class ComposePlaygroundActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val pages = listOf("One", "Two", "Three")
         setContent {
             WhatTheCodecTheme {
-                Box(
-                    Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
+                val pagerState = rememberPagerState(pageCount = pages.size)
+                Scaffold(
+                    topBar = {
+                        TopAppBar {
+                            Tabs(pages, pagerState)
+                        }
+                    }
                 ) {
-                    EmptyScreen(onVideoIconClick = { }, onAudioIconClick = {})
+                    Pager(pages, pagerState)
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun Tabs(pages: List<String>, pagerState: PagerState) {
+        val scope = rememberCoroutineScope()
+        TabRow(
+            // Our selected tab is our current page
+            selectedTabIndex = pagerState.currentPage,
+            // Override the indicator, using the provided pagerTabIndicatorOffset modifier
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                )
+            }
+        ) {
+            // Add tabs for all of our pages
+            pages.forEachIndexed { index, title ->
+                Tab(
+                    modifier = Modifier.fillMaxHeight(),
+                    text = { Text(title) },
+                    selected = pagerState.currentPage == index,
+                    onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(index)
+                        }
+                    },
+                )
+            }
+        }
+    }
+
+    @Composable
+    private fun Pager(pages: List<String>, pagerState: PagerState) {
+        HorizontalPager(state = pagerState) { page ->
+            Text(
+                modifier = Modifier.fillMaxSize(),
+                text = pages[page]
+            )
         }
     }
 }
