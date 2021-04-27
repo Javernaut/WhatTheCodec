fc5f38ef5b13c7bfcf543f1709d4ce0f121ddae7
Javernaut <javernaut@gmail.com>
2021-04-27 18:32:04 +0300

    Code reformatting

--- app/src/main/java/com/javernaut/whatthecodec/presentation/audio/ui/AudioPageFragment.kt
+++ app/src/main/java/com/javernaut/whatthecodec/presentation/audio/ui/AudioPageFragment.kt
@@ -1,7 +1,7 @@
 package com.javernaut.whatthecodec.presentation.audio.ui
 
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import android.view.View
 import com.javernaut.whatthecodec.R
 import com.javernaut.whatthecodec.domain.AudioStream
 import com.javernaut.whatthecodec.presentation.stream.BasePageFragment
@@ -10,34 +10,40 @@ import com.javernaut.whatthecodec.presentation.stream.model.makeStream
 
 class AudioPageFragment : BasePageFragment() {
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        mediaFileViewModel.audioStreamsLiveData.observe(viewLifecycleOwner, Observer {
+        mediaFileViewModel.audioStreamsLiveData.observe(viewLifecycleOwner) {
             displayStreams(it.map(::convertStream))
-        })
+        }
     }
 
     private fun convertStream(audioStream: AudioStream) =
-            makeStream(audioStream.basicInfo, resources) {
-                add(StreamFeature(R.string.page_audio_codec_name, audioStream.basicInfo.codecName))
+        makeStream(audioStream.basicInfo, resources) {
+            add(StreamFeature(R.string.page_audio_codec_name, audioStream.basicInfo.codecName))
 
-                if (audioStream.bitRate > 0) {
-                    add(StreamFeature(R.string.page_audio_bit_rate,
-                            BitRateHelper.toString(audioStream.bitRate, resources))
+            if (audioStream.bitRate > 0) {
+                add(
+                    StreamFeature(
+                        R.string.page_audio_bit_rate,
+                        BitRateHelper.toString(audioStream.bitRate, resources)
                     )
-                }
-                add(StreamFeature(R.string.page_audio_channels, audioStream.channels.toString()))
-
-                if (audioStream.channelLayout != null) {
-                    add(StreamFeature(R.string.page_audio_channel_layout, audioStream.channelLayout))
-                }
-                if (audioStream.sampleFormat != null) {
-                    add(StreamFeature(R.string.page_audio_sample_format, audioStream.sampleFormat))
-                }
-
-                add(StreamFeature(R.string.page_audio_sample_rate,
-                        SampleRateHelper.toString(audioStream.sampleRate, resources))
                 )
             }
+            add(StreamFeature(R.string.page_audio_channels, audioStream.channels.toString()))
+
+            if (audioStream.channelLayout != null) {
+                add(StreamFeature(R.string.page_audio_channel_layout, audioStream.channelLayout))
+            }
+            if (audioStream.sampleFormat != null) {
+                add(StreamFeature(R.string.page_audio_sample_format, audioStream.sampleFormat))
+            }
+
+            add(
+                StreamFeature(
+                    R.string.page_audio_sample_rate,
+                    SampleRateHelper.toString(audioStream.sampleRate, resources)
+                )
+            )
+        }
 }
--- app/src/main/java/com/javernaut/whatthecodec/presentation/subtitle/ui/SubtitlePageFragment.kt
+++ app/src/main/java/com/javernaut/whatthecodec/presentation/subtitle/ui/SubtitlePageFragment.kt
@@ -1,7 +1,7 @@
 package com.javernaut.whatthecodec.presentation.subtitle.ui
 
 import android.os.Bundle
-import androidx.lifecycle.Observer
+import android.view.View
 import com.javernaut.whatthecodec.R
 import com.javernaut.whatthecodec.domain.SubtitleStream
 import com.javernaut.whatthecodec.presentation.stream.BasePageFragment
@@ -10,16 +10,21 @@ import com.javernaut.whatthecodec.presentation.stream.model.makeStream
 
 class SubtitlePageFragment : BasePageFragment() {
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        mediaFileViewModel.subtitleStreamsLiveData.observe(viewLifecycleOwner, Observer {
+        mediaFileViewModel.subtitleStreamsLiveData.observe(viewLifecycleOwner) {
             displayStreams(it.map(::convertStream))
-        })
+        }
     }
 
     private fun convertStream(subtitleStream: SubtitleStream) =
-            makeStream(subtitleStream.basicInfo, resources) {
-                add(StreamFeature(R.string.page_subtitle_codec_name, subtitleStream.basicInfo.codecName))
-            }
+        makeStream(subtitleStream.basicInfo, resources) {
+            add(
+                StreamFeature(
+                    R.string.page_subtitle_codec_name,
+                    subtitleStream.basicInfo.codecName
+                )
+            )
+        }
 }
