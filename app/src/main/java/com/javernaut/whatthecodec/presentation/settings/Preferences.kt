3e2325e95a16ac6696eb69dd96121c5f8a3601a5
Javernaut <javernaut@gmail.com>
2021-05-16 13:33:32 +0300

    Settings. Minor Preference API refactoring

--- app/src/main/java/com/javernaut/whatthecodec/presentation/settings/Preferences.kt
+++ app/src/main/java/com/javernaut/whatthecodec/presentation/settings/Preferences.kt
@@ -1,5 +1,6 @@
 package com.javernaut.whatthecodec.presentation.settings
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -8,25 +9,89 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.RadioButton
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.preference.PreferenceManager
 import com.javernaut.whatthecodec.R
+import com.javernaut.whatthecodec.presentation.compose.theme.secondaryText
+
+@Composable
+fun ListPreference(
+    key: String,
+    defaultValue: String,
+    title: String,
+    displayableEntries: List<String>,
+    entriesCodes: List<String>,
+    onNewCodeSelected: (String) -> Unit
+) {
+    val applicationContext = LocalContext.current.applicationContext
+    val defaultSharedPreferences =
+        PreferenceManager.getDefaultSharedPreferences(applicationContext)
+    val selectedItemCode = defaultSharedPreferences.getString(key, defaultValue)
+
+    val currentlySelectedItemIndex = entriesCodes.indexOf(selectedItemCode)
+
+    var dialogOpened by remember { mutableStateOf(false) }
+    Preference(
+        title = title,
+        summary = displayableEntries[currentlySelectedItemIndex]
+    ) {
+        dialogOpened = true
+    }
+
+    if (dialogOpened) {
+        SingleChoicePreferenceDialog(
+            title = title,
+            onDismissRequest = { dialogOpened = false },
+            items = displayableEntries,
+            currentlySelectedIndex = currentlySelectedItemIndex
+        ) {
+            val newValueToSet = entriesCodes[it]
+            defaultSharedPreferences
+                .edit()
+                .putString(key, newValueToSet)
+                .apply()
+            onNewCodeSelected(newValueToSet)
+        }
+    }
+}
+
 
 @Composable
 fun SingleChoicePreferenceDialog(
     title: String,
     onDismissRequest: () -> Unit,
     items: List<String>,
-    currentlySelectedModeIndex: Int,
+    currentlySelectedIndex: Int,
     clickListener: (Int) -> Unit
 ) {
-    Dialog(title, onDismissRequest,
+    PreferenceDialog(title, onDismissRequest) {
+        items.forEachIndexed { index, item ->
+            PreferenceRadioButton(
+                item, index == currentlySelectedIndex
+            ) {
+                clickListener(index)
+                onDismissRequest()
+            }
+        }
+    }
+}
+
+@Composable
+private fun PreferenceDialog(
+    title: String,
+    onDismissRequest: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Dialog(
+        title, onDismissRequest,
         buttons = {
             TextButton(onClick = onDismissRequest) {
                 Text(
@@ -34,16 +99,8 @@ fun SingleChoicePreferenceDialog(
                     color = MaterialTheme.colors.onPrimary
                 )
             }
-        }) {
-        items.forEachIndexed { index, item ->
-            PreferenceRadioButton(
-                item, index == currentlySelectedModeIndex
-            ) {
-                clickListener(index)
-                onDismissRequest()
-            }
-        }
-    }
+        }, content = content
+    )
 }
 
 @Composable
@@ -82,5 +139,55 @@ fun PreferenceDivider() {
             .fillMaxWidth()
             .height(1.dp)
             .background(Color(if (isSystemInDarkTheme()) 0xFF282828 else 0xFFD9D9D9))
+    )
+}
+
+@Composable
+fun Preference(
+    @StringRes title: Int,
+    @StringRes summary: Int,
+    clickHandler: () -> Unit
+) =
+    Preference(
+        title = stringResource(id = title),
+        summary = stringResource(id = summary),
+        clickHandler = clickHandler
+    )
+
+@Composable
+fun Preference(
+    title: String,
+    summary: String,
+    clickHandler: () -> Unit
+) {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .clickable(onClick = clickHandler)
+            .padding(
+                start = 72.dp, top = 16.dp, end = 8.dp, bottom = 16.dp
+            )
+    ) {
+        Text(
+            title,
+            style = MaterialTheme.typography.subtitle1
+        )
+        Text(
+            summary,
+            style = MaterialTheme.typography.body2,
+            color = MaterialTheme.colors.secondaryText
+        )
+    }
+}
+
+@Composable
+fun PreferenceTitle(@StringRes title: Int) {
+    Text(
+        stringResource(id = title),
+        Modifier
+            .fillMaxWidth()
+            .padding(start = 72.dp, top = 24.dp, end = 8.dp, bottom = 8.dp),
+        style = MaterialTheme.typography.subtitle2,
+        color = MaterialTheme.colors.secondary
     )
 }
--- app/src/main/java/com/javernaut/whatthecodec/presentation/settings/SettingsActivity.kt
+++ app/src/main/java/com/javernaut/whatthecodec/presentation/settings/SettingsActivity.kt
@@ -8,24 +8,19 @@ import androidx.activity.compose.setContent
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.preference.PreferenceManager
 import com.javernaut.whatthecodec.R
 import com.javernaut.whatthecodec.presentation.compose.theme.WhatTheCodecTheme
-import com.javernaut.whatthecodec.presentation.compose.theme.secondaryText
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -92,90 +87,15 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     @Composable
-    private fun PreferenceTitle(@StringRes title: Int) {
-        Text(
-            stringResource(id = title),
-            Modifier
-                .fillMaxWidth()
-                .padding(start = 72.dp, top = 24.dp, end = 8.dp, bottom = 8.dp),
-            style = MaterialTheme.typography.subtitle2,
-            color = MaterialTheme.colors.secondary
-        )
-    }
-
-    @Composable
     private fun ThemeSelectionPreference() {
-        val applicationContext = LocalContext.current.applicationContext
-        val defaultSharedPreferences =
-            PreferenceManager.getDefaultSharedPreferences(applicationContext)
-        val currentNightMode = defaultSharedPreferences
-            .getString("theme", stringResource(id = R.string.settings_theme_default_pref_value))
-
-        val humanReadableNightModes = stringArrayResource(id = R.array.settings_theme_entries)
-        val modeCodes = stringArrayResource(id = R.array.settings_theme_entryValues)
-        val currentlySelectedModeIndex = modeCodes.indexOf(currentNightMode)
-
-        var dialogOpened by remember { mutableStateOf(false) }
-        val closeDialog = { dialogOpened = false }
-        Preference(
+        ListPreference(
+            "theme",
+            defaultValue = stringResource(id = R.string.settings_theme_default_pref_value),
             title = stringResource(id = R.string.settings_theme_title),
-            summary = humanReadableNightModes[currentlySelectedModeIndex]
+            displayableEntries = stringArrayResource(id = R.array.settings_theme_entries).toList(),
+            entriesCodes = stringArrayResource(id = R.array.settings_theme_entryValues).toList()
         ) {
-            dialogOpened = true
-        }
-
-        if (dialogOpened) {
-            SingleChoicePreferenceDialog(
-                title = stringResource(id = R.string.settings_theme_title),
-                onDismissRequest = closeDialog,
-                items = humanReadableNightModes.toList(),
-                currentlySelectedModeIndex = currentlySelectedModeIndex
-            ) {
-                val newValueToSet = modeCodes[it]
-                defaultSharedPreferences
-                    .edit()
-                    .putString("theme", newValueToSet)
-                    .apply()
-                ThemeManager.setNightModePreference(newValueToSet)
-            }
-        }
-    }
-
-    @Composable
-    private fun Preference(
-        @StringRes title: Int,
-        @StringRes summary: Int,
-        clickHandler: () -> Unit
-    ) =
-        Preference(
-            title = stringResource(id = title),
-            summary = stringResource(id = summary),
-            clickHandler = clickHandler
-        )
-
-    @Composable
-    private fun Preference(
-        title: String,
-        summary: String,
-        clickHandler: () -> Unit
-    ) {
-        Column(
-            Modifier
-                .fillMaxWidth()
-                .clickable(onClick = clickHandler)
-                .padding(
-                    start = 72.dp, top = 16.dp, end = 8.dp, bottom = 16.dp
-                )
-        ) {
-            Text(
-                title,
-                style = MaterialTheme.typography.subtitle1
-            )
-            Text(
-                summary,
-                style = MaterialTheme.typography.body2,
-                color = MaterialTheme.colors.secondaryText
-            )
+            ThemeManager.setNightModePreference(it)
         }
     }
 
