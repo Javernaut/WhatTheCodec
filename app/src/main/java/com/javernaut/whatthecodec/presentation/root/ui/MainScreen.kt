13c107aadf3cc9a24cdf04f77491f0c7da8265e8
Javernaut <javernaut@gmail.com>
2023-08-10 22:19:35 +0200

    Removing Accompanist

--- app/src/main/java/com/javernaut/whatthecodec/presentation/root/ui/MainScreen.kt
+++ app/src/main/java/com/javernaut/whatthecodec/presentation/root/ui/MainScreen.kt
@@ -11,15 +11,12 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScrollableTabRow
 import androidx.compose.material.Tab
-import androidx.compose.material.TabRowDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.pagerTabIndicatorOffset
 import com.javernaut.whatthecodec.R
 import com.javernaut.whatthecodec.presentation.audio.AudioPage
 import com.javernaut.whatthecodec.presentation.compose.common.WtcTopAppBar
@@ -53,7 +50,6 @@ fun MainScreen(
 
 @Composable
 @ExperimentalFoundationApi
-@OptIn(ExperimentalPagerApi::class)
 private fun MainScreenTopAppBar(
     tabsToShow: List<AvailableTab>,
     pagerState: PagerState,
@@ -65,13 +61,7 @@ private fun MainScreenTopAppBar(
             ScrollableTabRow(
                 edgePadding = 0.dp,
                 // Our selected tab is our current page
-                selectedTabIndex = pagerState.currentPage,
-                // Override the indicator, using the provided pagerTabIndicatorOffset modifier
-                indicator = { tabPositions ->
-                    TabRowDefaults.Indicator(
-                        Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
-                    )
-                }
+                selectedTabIndex = pagerState.currentPage
             ) {
                 // Add tabs for all of our pages
                 tabsToShow.forEachIndexed { index, tabToShow ->
