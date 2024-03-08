006e4c8b77d75c71e8bf646843f82efa59ae5d16
Javernaut <javernaut@gmail.com>
2024-03-08 12:33:52 +0100

    SettingsActivity now extend AppCompatActivity in order to automatically redraw after setting day/night mode manually

--- app/src/main/java/com/javernaut/whatthecodec/presentation/settings/SettingsActivity.kt
+++ app/src/main/java/com/javernaut/whatthecodec/presentation/settings/SettingsActivity.kt
@@ -4,13 +4,13 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.browser.customtabs.CustomTabsIntent
 import com.javernaut.whatthecodec.presentation.compose.theme3.WhatTheCodecM3Theme
 
-class SettingsActivity : ComponentActivity() {
+class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
