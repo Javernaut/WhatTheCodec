21cdeb01dd9f9407ab2f13cc701f42527225eebd
Javernaut <javernaut@gmail.com>
2024-03-19 09:06:23 +0100

    Disabling the status bar coloring via the Compose theming

--- app/src/main/java/com/javernaut/whatthecodec/presentation/compose/theme3/Theme.kt
+++ app/src/main/java/com/javernaut/whatthecodec/presentation/compose/theme3/Theme.kt
@@ -1,13 +1,8 @@
 package com.javernaut.whatthecodec.presentation.compose.theme3
 
-import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
 
 
 @Composable
@@ -16,15 +11,6 @@ fun WhatTheCodecM3Theme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = if (darkTheme) darkScheme else lightScheme
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
-        }
-    }
-
     MaterialTheme(
         colorScheme = colorScheme,
         content = content
