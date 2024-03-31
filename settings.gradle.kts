5c897f0a91a1d6c1ee40dfb990112a3d2e5e6aad
Javernaut <javernaut@gmail.com>
2024-03-31 18:07:33 +0200

    settings.gradle groovy -> kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+import java.net.URI
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            name = "MaventCentralSnapshots"
+            url = URI("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        }
+    }
+}
+
+rootProject.name = "WhatTheCodec"
+include(":app")
+ 
