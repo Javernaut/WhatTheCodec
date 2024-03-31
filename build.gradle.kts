9559b099a3b078e387899138b528ecc84511732b
Javernaut <javernaut@gmail.com>
2024-03-31 18:10:52 +0200

    root build.gradle groovy -> kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,4 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+// TODO Migrate to the Secrets Plugin
+apply(from = "${rootDir}/scripts/read-arguments.gradle")
