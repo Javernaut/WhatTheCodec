0dc52fc90c684fb588d040e0983c4a4e4c3cb810
Javernaut <javernaut@gmail.com>
2024-05-05 17:10:34 +0200

    Initial video page screenshot

--- app/src/androidTest/java/com/javernaut/whatthecodec/screenshots/ScreenshotsTestSuite.kt
+++ app/src/androidTest/java/com/javernaut/whatthecodec/screenshots/ScreenshotsTestSuite.kt
@@ -12,9 +12,17 @@ import com.javernaut.whatthecodec.home.data.completeEnumSet
 import com.javernaut.whatthecodec.home.data.model.AudioStreamFeature
 import com.javernaut.whatthecodec.home.data.model.SubtitleStreamFeature
 import com.javernaut.whatthecodec.home.data.model.VideoStreamFeature
+import com.javernaut.whatthecodec.home.presentation.model.AudioPage
+import com.javernaut.whatthecodec.home.presentation.model.NoPreviewAvailable
+import com.javernaut.whatthecodec.home.presentation.model.ScreenState
+import com.javernaut.whatthecodec.home.presentation.model.SubtitlesPage
+import com.javernaut.whatthecodec.home.presentation.model.VideoPage
 import com.javernaut.whatthecodec.home.ui.screen.EmptyHomeScreen
+import com.javernaut.whatthecodec.home.ui.screen.MainHomeScreen
 import com.javernaut.whatthecodec.settings.presentation.SettingsViewModel
 import com.javernaut.whatthecodec.settings.ui.SettingsScreen
+import io.github.javernaut.mediafile.BasicStreamInfo
+import io.github.javernaut.mediafile.VideoStream
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -50,7 +58,6 @@ class ScreenshotsTestSuite(
 
     // TODO Use 3 button navigation for dark mode and gesture navigation for light mode
     // TODO Use the Demo Mode (status bar clearing - no notifications)
-    // TODO Status bar should have proper color
     // TODO Tests should run on 2 specific devices and screenshots have to be grabbed from all of them
     @Test
     fun emptyScreen() {
@@ -61,6 +68,39 @@ class ScreenshotsTestSuite(
                 onSettingsClicked = { },
                 screenMassages = emptyFlow()
             )
+        }
+    }
+
+    @Test
+    fun videoTabScreen() {
+        val basicStreamInfo = mockk<BasicStreamInfo>()
+        every { basicStreamInfo.index } returns 0
+        every { basicStreamInfo.title } returns null
+        every { basicStreamInfo.codecName } returns "Super"
+        every { basicStreamInfo.language } returns null
+        every { basicStreamInfo.disposition } returns 0
+
+        val videoStream = mockk<VideoStream>()
+        every { videoStream.basicInfo } returns basicStreamInfo
+        every { videoStream.frameHeight } returns 100
+        every { videoStream.frameWidth } returns 100
+        every { videoStream.frameRate } returns 100.0
+        every { videoStream.bitRate } returns 100
+
+        val screenState = ScreenState(
+            VideoPage(
+                NoPreviewAvailable,
+                "fileFormat",
+                true,
+                videoStream,
+                videoStreamFeatures = completeEnumSet()
+            ),
+            // These 2 are not visible anyway
+            AudioPage(streams = emptyList(), streamFeatures = emptySet()),
+            SubtitlesPage(streams = emptyList(), streamFeatures = emptySet())
+        )
+        makeScreenshotOf("video") {
+            MainHomeScreen(screenState, {}, {}, {}, {}, emptyFlow())
         }
     }
 
