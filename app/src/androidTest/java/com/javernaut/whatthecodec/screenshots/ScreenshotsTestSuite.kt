aed9a4b2abb39fa24ff0e32ef24e10016c5a5894
Javernaut <javernaut@gmail.com>
2024-05-05 15:18:58 +0200

    Using @Parameterized test for dark/light themes applying

--- app/src/androidTest/java/com/javernaut/whatthecodec/screenshots/ScreenshotsTestSuite.kt
+++ app/src/androidTest/java/com/javernaut/whatthecodec/screenshots/ScreenshotsTestSuite.kt
@@ -18,8 +18,16 @@ import org.junit.runners.Parameterized.Parameters
 import tools.fastlane.screengrab.Screengrab
 import tools.fastlane.screengrab.locale.LocaleTestRule
 
-@RunWith(AndroidJUnit4::class)
-class ScreenshotsTestSuite {
+@RunWith(Parameterized::class)
+class ScreenshotsTestSuite(
+    private val darkMode: Boolean
+) {
+    companion object {
+        @JvmStatic
+        @Parameters
+        fun parameters() = listOf(true, false)
+    }
+
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
@@ -31,18 +39,17 @@ class ScreenshotsTestSuite {
     val mRuntimePermissionRule: GrantPermissionRule =
         GrantPermissionRule.grant("android.permission.DUMP")
 
-    // TODO Try parametrized tests here to pick the theme and the screenshots suffix
     // TODO Use 3 button navigation for dark mode and gesture navigation for light mode
     // TODO Use the Demo Mode (status bar clearing)
     // TODO Status bar should have proper color
     // TODO Tests should run on 2 specific devices and screenshots have to be grabbed from all of them
     @Test
-    fun emptyScreenLight() {
+    fun emptyScreen() {
         composeTestRule.activityRule.scenario.onActivity {
             it.enableEdgeToEdge()
         }
         composeTestRule.setContent {
-            WhatTheCodecTheme.Static(darkTheme = false) {
+            WhatTheCodecTheme.Static(darkTheme = darkMode) {
                 EmptyHomeScreen(
                     onVideoIconClick = { },
                     onAudioIconClick = { },
@@ -52,25 +59,8 @@ class ScreenshotsTestSuite {
             }
         }
         composeTestRule.waitForIdle()
-        Screengrab.screenshot("empty_light")
-    }
 
-    @Test
-    fun emptyScreenDark() {
-        composeTestRule.activityRule.scenario.onActivity {
-            it.enableEdgeToEdge()
-        }
-        composeTestRule.setContent {
-            WhatTheCodecTheme.Static(darkTheme = true) {
-                EmptyHomeScreen(
-                    onVideoIconClick = { },
-                    onAudioIconClick = { },
-                    onSettingsClicked = { },
-                    screenMassages = emptyFlow()
-                )
-            }
-        }
-        composeTestRule.waitForIdle()
-        Screengrab.screenshot("empty_dark")
+        val suffix = if (darkMode) "dark" else "light"
+        Screengrab.screenshot("empty_$suffix")
     }
 }
