80d9b761e7a7099e17c25bc4948e9f040f625b39
Javernaut <javernaut@gmail.com>
2024-08-12 19:44:13 +0200

    Updating Baseline and Startup Profiles

--- baselineprofile/src/main/kotlin/com/javernaut/whatthecodec/baselineprofile/BaselineProfileGenerator.kt
+++ baselineprofile/src/main/kotlin/com/javernaut/whatthecodec/baselineprofile/BaselineProfileGenerator.kt
@@ -11,20 +11,6 @@ import org.junit.runner.RunWith
 /**
  * This test class generates a basic startup baseline profile for the target package.
  *
- * We recommend you start with this but add important user flows to the profile to improve their performance.
- * Refer to the [baseline profile documentation](https://d.android.com/topic/performance/baselineprofiles)
- * for more information.
- *
- * You can run the generator with the "Generate Baseline Profile" run configuration in Android Studio or
- * the equivalent `generateBaselineProfile` gradle task:
- * ```
- * ./gradlew :app:generateReleaseBaselineProfile
- * ```
- * The run configuration runs the Gradle task and applies filtering to run only the generators.
- *
- * Check [documentation](https://d.android.com/topic/performance/benchmarking/macrobenchmark-instrumentation-args)
- * for more information about available instrumentation arguments.
- *
  * After you run the generator, you can verify the improvements running the [StartupBenchmarks] benchmark.
  *
  * When using this class to generate a baseline profile, only API 33+ or rooted API 28+ are supported.
--- baselineprofile/src/main/kotlin/com/javernaut/whatthecodec/baselineprofile/StartupBenchmarks.kt
+++ baselineprofile/src/main/kotlin/com/javernaut/whatthecodec/baselineprofile/StartupBenchmarks.kt
@@ -18,19 +18,8 @@ import org.junit.runner.RunWith
  * It does this by comparing [CompilationMode.None], which represents the app with no Baseline
  * Profiles optimizations, and [CompilationMode.Partial], which uses Baseline Profiles.
  *
- * Run this benchmark to see startup measurements and captured system traces for verifying
- * the effectiveness of your Baseline Profiles. You can run it directly from Android
- * Studio as an instrumentation test, or run all benchmarks for a variant, for example benchmarkRelease,
- * with this Gradle task:
- * ```
- * ./gradlew :baselineprofile:connectedBenchmarkReleaseAndroidTest
- * ```
- *
  * You should run the benchmarks on a physical device, not an Android emulator, because the
  * emulator doesn't represent real world performance and shares system resources with its host.
- *
- * For more information, see the [Macrobenchmark documentation](https://d.android.com/macrobenchmark#create-macrobenchmark)
- * and the [instrumentation arguments documentation](https://d.android.com/topic/performance/benchmarking/macrobenchmark-instrumentation-args).
  **/
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -61,15 +50,6 @@ class StartupBenchmarks {
             },
             measureBlock = {
                 startActivityAndWait()
-
-                // TODO Add interactions to wait for when your app is fully drawn.
-                // The app is fully drawn when Activity.reportFullyDrawn is called.
-                // For Jetpack Compose, you can use ReportDrawn, ReportDrawnWhen and ReportDrawnAfter
-                // from the AndroidX Activity library.
-
-                // Check the UiAutomator documentation for more information on how to
-                // interact with the app.
-                // https://d.android.com/training/testing/other-components/ui-automator
             }
         )
     }
