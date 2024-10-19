8215ddd4126ce84d5b7e26b9cc3d8b7edf72fb4b
Javernaut <javernaut@gmail.com>
2024-10-19 23:06:57 +0200

    Introducing a MediaFileContext with a saved handle to AVFormatContext and a way to dispose it.

--- app/src/debug/java/com/javernaut/whatthecodec/compose/playground/ComposePlaygroundActivity.kt
+++ app/src/debug/java/com/javernaut/whatthecodec/compose/playground/ComposePlaygroundActivity.kt
@@ -1,19 +1,26 @@
 package com.javernaut.whatthecodec.compose.playground
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.net.toUri
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.javernaut.whatthecodec.compose.theme.WhatTheCodecTheme
 import com.javernaut.whatthecodec.home.presentation.MediaFileArgument
-import com.javernaut.whatthecodec.home.presentation.MediaFileViewModel
 import com.javernaut.whatthecodec.home.ui.screen.EmptyHomeScreen
 import com.javernaut.whatthecodec.home.ui.screen.pickAudioFile
 import com.javernaut.whatthecodec.home.ui.screen.pickVideoFile
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.javernaut.mediafile.creator.MediaType
+import io.github.javernaut.mediafile.factory.MediaFileFactory
+import io.github.javernaut.mediafile.factory.Request
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ComposePlaygroundActivity : ComponentActivity() {
@@ -24,7 +31,7 @@ class ComposePlaygroundActivity : ComponentActivity() {
 
         setContent {
             WhatTheCodecTheme.Dynamic {
-                val viewModel = hiltViewModel<MediaFileViewModel>()
+                val viewModel = hiltViewModel<TestMediaFileViewModel>()
 
                 val onAudioIconClick = pickAudioFile(
                     permissionDenied = viewModel::onPermissionDenied
@@ -56,6 +63,24 @@ class ComposePlaygroundActivity : ComponentActivity() {
                     }, emptyFlow()
                 )
             }
+        }
+    }
+}
+
+class TestMediaFileViewModel : ViewModel() {
+    fun onPermissionDenied() {
+        // Whatever, it's test
+    }
+
+    fun openMediaFile(mediaFileArgument: MediaFileArgument) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val mediaFileContext = MediaFileFactory.create(
+                Request.Content(mediaFileArgument.uri.toUri()), mediaFileArgument.type
+            )
+
+            Log.e("MediaFileFactory", "is mediaFileContext null? ${mediaFileContext == null}")
+
+            mediaFileContext?.dispose()
         }
     }
 }
