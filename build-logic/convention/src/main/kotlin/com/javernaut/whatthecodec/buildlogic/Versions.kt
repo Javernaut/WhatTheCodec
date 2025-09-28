c2d61d487d555884aed3d463bde90d979ba4f15d
Javernaut <javernaut@gmail.com>
2025-09-28 07:54:42 +0200

    Updating the compile and target sdk to 36

--- build-logic/convention/src/main/kotlin/com/javernaut/whatthecodec/buildlogic/Versions.kt
+++ build-logic/convention/src/main/kotlin/com/javernaut/whatthecodec/buildlogic/Versions.kt
@@ -4,8 +4,8 @@ import org.gradle.api.JavaVersion
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 object Versions {
-    const val targetSdk = 34
-    const val compileSdk = 35
+    const val targetSdk = 36
+    const val compileSdk = 36
     const val minSdk = 24
 
     val javaSourceCompatibility = JavaVersion.VERSION_17
